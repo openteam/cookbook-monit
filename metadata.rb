3e5a529b5e234b3bbbfb1703f882ff9e7afc46a5
tab <tukhfatullin@gmail.com>
2014-03-26 14:06:55 +0700

    bump version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'tab@openteam.ru'
 license          'All rights reserved'
 description      'Installs and configures monit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
